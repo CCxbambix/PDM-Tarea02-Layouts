7f81ca6224ea41fe9b24672312d583a02794140c
CCxbambix <luisalbertosalazar@ciencias.unam.mx>
2026-03-19 22:34:07 -0600

    Terminado las dos layouts

---
+++ aplicacion/app/src/main/java/com/example/tarea2layouts/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tarea2layouts;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //Comente una linea para poder ver la otra layout
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chilaquiles);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
